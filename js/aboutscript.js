2ed48361bf9a2d69e203e7707fb05beab5f8ba9a
sohaibahsan007 <sohaibahsan007@live.com>
2016-09-06 19:12:20 +0500

    dfgdg

--- js/aboutscript.js
+++ js/aboutscript.js
@@ -32,24 +32,3 @@ $(document).ready(function(){
     });
   });
 });
-var myCenter = new google.maps.LatLng(31.388415, 73.025928);
-
-function initialize() {
-var mapProp = {
-  center:myCenter,
-  zoom:14,
-  scrollwheel:false,
-  draggable:false,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-var marker = new google.maps.Marker({
-  position:myCenter,
-  });
-
-marker.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
